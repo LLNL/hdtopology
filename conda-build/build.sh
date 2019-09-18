88a540111a7a77cb9f3a4fb0e18baffa89b05cab
shusenl <shusenl@sci.utah.edu>
2019-09-18 00:07:23 -0700

    add travis setup

--- conda-build/build.sh
+++ conda-build/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # very verbose
 set -x
 
@@ -11,53 +10,39 @@ set -e
 set -o pipefail
 
 # cmake flags
-export PYTHON_VERSION=${PYTHON_VERSION:-3.6.1}
+export PYTHON_VERSION=${PYTHON_VERSION:-3.7.4}
 
 # assume miniconda installed here
 export MINICONDA_ROOT=${HOME}/miniconda${PYTHON_VERSION:0:1}
 
 if [ "$(uname)" == "Darwin" ]; then
-	OSX=1
+    OSX=1
 fi
+
+# INSTALL_CONDA=1
 
 # install conda
 if (( INSTALL_CONDA == 1 )) ; then
+    # install Miniconda
+    if [ ! -d  ${MINICONDA_ROOT} ]; then
+        pushd ${HOME}
+        if (( OSX == 1 )) ; then
+            __name__=MacOSX-x86_64
+        else
+            __name__=Linux-x86_64
+        fi
 
-	# check I have a portable SDK
-	# here I need sudo!
-	if (( OSX ==  1 )) ; then
-		if [ ! -d /opt/MacOSX10.9.sdk ] ; then
-		  if (( IsRoot == 1 )) ; then
-				git clone  https://github.com/phracker/MacOSX-SDKs.git
-				sudo mv MacOSX-SDKs/MacOSX10.9.sdk /opt/
-				rm -Rf MacOSX-SDKs
-			else
-				echo "Missing /opt/MacOSX10.9.sdk, but to install it I need sudo"
-				exit -1
-			fi
-		fi
-	fi
+        url="https://repo.continuum.io/miniconda/Miniconda${PYTHON_VERSION:0:1}-latest-${__name__}.sh"
+        curl -fsSL --insecure ${url} -o miniconda.sh
+        chmod +x miniconda.sh
+        ./miniconda.sh -b
+        popd
+    fi
 
-	# install Miniconda
-	if [ ! -d  ${MINICONDA_ROOT} ]; then
-		pushd ${HOME}
-		if (( OSX == 1 )) ; then
-			__name__=MacOSX-x86_64
-		else
-			__name__=Linux-x86_64
-		fi
-
-		url="https://repo.continuum.io/miniconda/Miniconda${PYTHON_VERSION:0:1}-latest-${__name__}.sh"
-		curl -fsSL --insecure ${url} -o miniconda.sh
-		chmod +x miniconda.sh
-		./miniconda.sh -b
-		popd
-	fi
-
-	export PATH=${MINICONDA_ROOT}/bin:$PATH
-	conda config  --set changeps1 no --set anaconda_upload no
-	conda update  --yes conda
-	conda install --yes -q conda-build anaconda-client
+    export PATH=${MINICONDA_ROOT}/bin:$PATH
+    conda config  --set changeps1 no --set anaconda_upload no
+    conda update  --yes conda
+    conda install --yes -q conda-build anaconda-client
 
 fi
 
@@ -72,26 +57,25 @@ conda install --yes numpy
 
 # see conda/hdtopology/build.sh
 if (( 1 == 1 )) ; then
-	pushd ./conda
-	conda-build --python=${PYTHON_VERSION} -q hdtopology
-	conda install --yes -q --use-local hdtopology
-	popd
+    # pushd ./conda
+    conda-build --python=${PYTHON_VERSION} -q hdtopology
+    conda install --yes -q --use-local hdtopology
+    # popd
 fi
 
 # test
 if (( 1 == 1 )) ; then
-	pushd $(python -m hdtopology dirname)
-	python Samples/python/Array.py
-	python Samples/python/Dataflow.py
-	python Samples/python/Idx.py
-	popd
+    pushd $(python -m hdtopology dirname)
+    python utilities/testWriteSummaryTopology.py
+    python utilities/testReadSummaryTopology.py
+    popd
 fi
 
 # the deploy happens here for the top-level build
 if [[ "${TRAVIS_TAG}" != "" ]] ; then
-	CONDA_BUILD_FILENAME=$(find ${MINICONDA_ROOT}/conda-bld -iname "hdtopology*.tar.bz2")
-	echo "Doing deploy to anaconda ${CONDA_BUILD_FILENAME}..."
-	anaconda -t ${ANACONDA_TOKEN} upload "${CONDA_BUILD_FILENAME}"
+    CONDA_BUILD_FILENAME=$(find ${MINICONDA_ROOT}/conda-bld -iname "hdtopology*.tar.bz2")
+    echo "Doing deploy to anaconda ${CONDA_BUILD_FILENAME}..."
+    anaconda -t ${ANACONDA_TOKEN} upload "${CONDA_BUILD_FILENAME}"
 fi
 
 echo "hdtopology build_conda.sh finished"
