5665a9cef13caaa25a34aecd60882af52fd236d9
shusenl <shusenl@sci.utah.edu>
2019-09-19 19:29:40 -0700

    update travis script

--- travis/travis_build.sh
+++ travis/travis_build.sh
@@ -4,6 +4,8 @@ set -e -x
 # Install dependent library
 yum install -y ann-devel
 
+# Install cmake
+yum install wget
 wget http://www.cmake.org/files/v3.12/cmake-3.12.1.tar.gz
 tar -xvzf cmake-3.12.1.tar.gz
 cd cmake-3.12.1/
