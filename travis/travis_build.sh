a9f3d084cf52ac7de285e5e7cfc0b00f7a784a50
Shusen Liu <liu42@llnl.gov>
2019-09-26 19:47:31 -0700

    update travis

--- travis/travis_build.sh
+++ travis/travis_build.sh
@@ -6,12 +6,16 @@ yum install -y ann-devel
 
 # Install cmake
 yum install -y wget
-wget http://www.cmake.org/files/v3.12/cmake-3.12.1.tar.gz
+wget --secure-protocol=TLSv1  http://www.cmake.org/files/v3.12/cmake-3.12.1.tar.gz
 tar -xvzf cmake-3.12.1.tar.gz
 cd cmake-3.12.1/
 ./configure
 make
 sudo make install
+
+#git clone https://github.com/dials/annlib.git
+#cd /io/annlib/
+#make linux-g++
 
 mkdir /io/build
 cd /io/build
