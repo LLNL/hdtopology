9fd4317004b0efd278a0ffee395465d739dc0ad3
Shusen Liu <liu42@llnl.gov>
2019-09-26 19:29:48 -0700

    update travis

--- travis/travis_build.sh
+++ travis/travis_build.sh
@@ -5,7 +5,7 @@ set -e -x
 yum install -y ann-devel
 
 # Install cmake
-yum install wget
+yum install -y wget
 wget http://www.cmake.org/files/v3.12/cmake-3.12.1.tar.gz
 tar -xvzf cmake-3.12.1.tar.gz
 cd cmake-3.12.1/
