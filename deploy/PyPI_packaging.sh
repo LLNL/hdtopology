2df649edc1340a18e58c7cd89e297a0b94d8126d
shusenl <shusen.liu.hust@gmail.com>
2021-04-29 16:09:00 +0000

    change deploy script

--- deploy/PyPI_packaging.sh
+++ deploy/PyPI_packaging.sh
@@ -9,23 +9,28 @@ rm -f hdtopology/dist/*
 rm -f ngl/dist/*
 
 ###### packaging for PyPI ######
+cd hdtopology
+python3 setup.py sdist bdist_wheel
+for wheel in $(find . -iname "*.whl") ; do 
+  mv $wheel $(echo $wheel | sed 's/-linux_/-manylinux1_/')
+done
+python3 -m twine upload --repository-url https://test.pypi.org/legacy/ --verbose dist/*
+cd ..
+exit 1
+
 cd ngl
 python3 setup.py sdist bdist_wheel
 for wheel in $(find . -iname "*.whl") ; do 
   mv $wheel $(echo $wheel | sed 's/-linux_/-manylinux1_/')
 done
 python3 -m twine upload --repository-url https://test.pypi.org/legacy/ --verbose dist/*
+cd ..
 
-cd ../hdff
+cd hdff
 python3 setup.py sdist bdist_wheel
 for wheel in $(find . -iname "*.whl") ; do 
   mv $wheel $(echo $wheel | sed 's/-linux_/-manylinux1_/')
 done
 python3 -m twine upload --repository-url https://test.pypi.org/legacy/ --verbose dist/*
+cd ..
 
-cd ../hdtopology
-python3 setup.py sdist bdist_wheel
-for wheel in $(find . -iname "*.whl") ; do 
-  mv $wheel $(echo $wheel | sed 's/-linux_/-manylinux1_/')
-done
-python3 -m twine upload --repository-url https://test.pypi.org/legacy/ --verbose dist/*
