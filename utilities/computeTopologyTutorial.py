975a13ceac4f492b862243a404a7fff751c865b9
Peer-Timo Bremer <bremer5@llnl.gov>
2021-10-21 11:41:47 -0700

    Fixed bugs in graph construction

--- utilities/computeTopologyTutorial.py
+++ utilities/computeTopologyTutorial.py
@@ -15,6 +15,9 @@ data = np.fromfile(argv[1],dtype=np.float32)
 dimx = int(argv[2])
 dimy = int(argv[3])
 
+#data = data[0:9]
+
+
 img = data.reshape([dimy,dimx])
 
 # Now create a 2D mesh to put the function on 
@@ -26,18 +29,21 @@ names = ['X', 'Y', 'f']
 types = ['f4']*3
 data = data.view(dtype=list(zip(names,types)) ).view(np.recarray)
 
-
 edges = []
 for j in range(0,dimy-1):
     for i in range(0,dimx-1):
         edges.append([j*dimx + i, j*dimx + i + 1])
         edges.append([j*dimx + i, (j+1)*dimx + i])
-    
-    edges.append([j*dimx, (j+1)*dimx + dimx - 1])
+        
+    edges.append([j*dimx + dimx - 1, (j+1)*dimx + dimx - 1])
 
 for i in range(0,dimx-1):
     edges.append([(dimy-1)*dimx + i, (dimy-1)*dimx + i + 1])
-   
+
+# Now we make sure that the edges are symmetric
+edges2 = [[e[1],e[0]] for e in edges]
+edges += edges2
+
 edges = np.array(edges,dtype=np.uint32)    
 
 
@@ -69,7 +75,7 @@ plt.imshow(discrete.reshape([dimy,dimx]))
 
 
 # Get k number of segments
-segs = eg.segmentation(5)
+segs = eg.segmentation(6)
 
 for k,s in enumerate(segs):
     for i in s:
